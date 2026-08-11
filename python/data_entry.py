903e5230f926364bd4977d5f1d51290a7a91b058
muhd-jagfer <muhdjagfer@gmail.com>
2026-08-11 20:05:55 +0530

    alternation

---
+++ python/data_entry.py
@@ -0,0 +1,36 @@
+from datetime import datetime
+
+date_format = "%d-%m-%Y"
+CATEGORIES = {"I": "Income", "E": "Expense"}
+
+def get_date(prompt, allow_default = False):
+    date_str = input(prompt).strip()
+    if allow_default and not date_str:
+        return datetime.today().strftime(date_format)
+
+    try:
+        valid_date = datetime.strptime(date_str, date_format)
+        return valid_date.strftime(date_format)
+    except ValueError:
+        print("Invalid date format, please enter the date in dd-mm-YYYY")
+        return get_date(prompt, allow_default)
+
+def get_amount():
+    try:
+        amount = float(input("Enter an amount: "))
+        if amount <= 0:
+            raise ValueError("Amount must be a positive number")
+        return amount
+    except ValueError as e:
+        print(e)
+        return get_amount()
+
+def get_category():
+    while True:
+        category = input("Enter the category (I for Income or E for Expense): ").strip().upper()
+        if category in CATEGORIES:
+            return CATEGORIES[category]
+        print("Invalid category. Please enter I for Income or E for Expense.")
+
+def get_description():
+    return input("Enter a description (optional): ")
